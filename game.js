164907b94cfc7beaecd0efd118184220100819ce
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2023-01-04 22:15:34 -0600

    stopped at the paddle.js file

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 //Update Loop
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 const ball = new Ball(document.getElementById("ball"));
-
-//This Function is basically a loop as it calls the "window.requestAnimationFrame()" function which calls the "update" function, pretty working as a Recursive Function
+const player1Paddle = new Paddle(document.getElementById("player1-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
+//This Function is basically a loop as it calls the "window.requestAnimationFrame()" function which calls the "update" function, pretty working as a Recursive Function
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
@@ -15,5 +17,10 @@ function update(time) {
   lastTime = time;
   window.requestAnimationFrame(update);
 }
+
+//This eventListener helps determine the position of paddle within the window (game arena) of the game
+document.addEventListener("mousemove", e => {
+  player1Paddle.position = (e.y / window.innerHeight) * 100;
+});
 
 window.requestAnimationFrame(update);
